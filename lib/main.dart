e013da1dbd949c1ba0260a06bd2f6c65421da9a3
Nesarahmad <nasarahmemo12345@gmail.com>
2026-08-02 12:19:53 +0430

    creating the project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(diceApp());
+}
+class diceApp extends StatefulWidget{
+  @override
+  State<diceApp> createState() => _diceAppState();
+}
+
+class _diceAppState extends State<diceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+   
+    );
+  }
+}
