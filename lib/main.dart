189e18293b9d2f1ff1899619b49a9582a3138efd
Nesarahmad <nasarahmemo12345@gmail.com>
2026-08-02 13:36:52 +0430

    making the first imaages

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,33 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(diceApp());
+  runApp(DiceApp());
 }
-class diceApp extends StatefulWidget{
+class DiceApp extends StatefulWidget{
+  
   @override
-  State<diceApp> createState() => _diceAppState();
+  State<DiceApp> createState() => _DiceAppState();
 }
 
-class _diceAppState extends State<diceApp> {
+class _DiceAppState extends State<DiceApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-   
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 4, 86, 226),
+         appBar: AppBar(
+      backgroundColor: Colors.blueAccent[700],
+        title: Text('Dice App',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+      ),
+      body: Center(
+        child: Row(
+          children:[
+            Expanded(child: Image.asset('assets/images/dice1.png'),),
+            Expanded(child: Image.asset('assets/images/dice2.png'),),
+          ]
+        ),
+      ),
+      ),
     );
   }
 }
